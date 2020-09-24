e766caa7b6ea918f78f09a5afa66a44e0a2a280c
mitragroups <miftahmuhajir@gmail.com>
2020-09-24 23:47:48 +0700

    Ruby on Rails Application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: 'Buku Sinatra', page: 45, price: 45000, description: 'Salah satu framework Ruby')
+Book.create(title: 'Buku Java', page: 120, price: 120000, description: 'Java solid programming')
+Book.create(title: 'Buku React Native', page: 85, price: 85000, description: 'Framework frontend JS')
+Book.create(title: 'Buku Kotlin', page: 50, price: 50000, description: 'Bahasa kedua android')
+Book.create(title: 'Buku Swift', page: 30, price: 30000, description: 'Untuk iOS')
